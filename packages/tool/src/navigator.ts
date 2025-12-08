b867c8d177ffb8ddfc881c31b11ac3c7d70e1300
devil <devilchinaqin@gmail.com>
2025-12-08 10:25:13 +0800

    feat:12.8

--- packages/tool/src/navigator.ts
+++ packages/tool/src/navigator.ts
@@ -1,15 +1,9 @@
 import { useNavigate } from 'react-router-dom'
-import { getEnv, navigatorHandler } from '@brushes/utils';
+
 export const useNavigateImpl = () => {
     const toPath = useNavigate();
-    const isTaro = getEnv();
     const navigator = (path:string | number) => {
         if(!path) return;
-        console.log(1111, isTaro);
-        if(isTaro) {
-            navigatorHandler('goodPoints');
-            return;
-        }
         if(typeof path !== 'number' && path.startsWith('http')) {
             window.open(path, '_blank');
         } else {
